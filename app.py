134f5ef1b9a7b201864d672413eaa15eaf24ace1
Tom Cabanski <tom@cabanski.com>
2022-10-01 21:39:00 -0500

    Roughing in ideas for how monsters get added

--- app.py
+++ app.py
@@ -17,14 +17,37 @@ intents.message_content = True
 
 bot = commands.Bot(intents=intents, command_prefix='@')
 
+encounter = {
+    'round': 0, 
+    'members': []
+}
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='start')
 async def xx(ctx):
-    encounter = json.loads(pyperclip.paste())
+    encounter = {
+        'round': 0, 
+        'members': []
+    }
     await ctx.message.author.send(f'Hello from bot {encounter}')
     await ctx.send(f'Hello from bot {encounter}')
+
+@bot.command(name ='add')
+async def add(ctx, name: str, hp: int, ac: int, init: str):
+    if init == None:
+        init = '+0'
+
+    encounter['members'].append({
+        "init": init,
+        "name": name,
+        "hp": hp,
+        "ac": ac
+    })
+    await ctx.message.author.send(f'Add complete {encounter}')
+    await ctx.send(f'Add complete {encounter}')
+
 
 bot.run(TOKEN)
