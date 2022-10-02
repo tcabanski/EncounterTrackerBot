9d2361ffb029b4f25d432c6a13b1af4319256942
Tom Cabanski <tom@cabanski.com>
2022-10-01 20:49:37 -0500

    Use DM channel for the DM

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # bot.py
+from email import message
 import os
 import random
 import discord
@@ -14,15 +15,16 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(intents=intents, command_prefix='!')
+bot = commands.Bot(intents=intents, command_prefix='@')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command(name='xx')
+@bot.command(name='start')
 async def xx(ctx):
     encounter = json.loads(pyperclip.paste())
+    await ctx.message.author.send(f'Hello from bot {encounter}', )
     await ctx.send(f'Hello from bot {encounter}')
 
 bot.run(TOKEN)
